55e2e615c7dfcbab23449d483224c7d71dc5fe29
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-16 13:50:20 +0300

    Добавил решение задачи, которая выводит на экран содержание массива и сумму элементов массива, стоящих на нечетных позициях

---
+++ 08Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+double[] Array = new double[20];
+for(int i = 0; i<Array.Length; i++)
+{
+    Array[i] = new Random().Next(1,11);
+    Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
+double sum = 0;
+for(int i = 1; i<Array.Length; i++)
+{
+    if(i%2!=0)sum = sum + Array[i];
+}
+Console.WriteLine($"Сумма элементов с нечетными номерами - {sum}");

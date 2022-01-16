7aee1e596c7fa6c6e19b196ec4522074956b894f
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-16 13:01:13 +0300

    Программа выдает количество четных и нечетных чисел в массиве. Каждый элемент массива генерируется генератором случайных чисел от 100 до 999

---
+++ 06Task/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+double[] Array = new double[20];
+for(int i = 0; i<Array.Length; i++)
+{
+    Array[i] = new Random().Next(100,1000);
+    Console.Write($"{Array[i]} ");
+}
+int countEven = 0;
+int countNotEven = 0;
+for(int i = 0; i< Array.Length; i++)
+{
+    if(Array[i]%2==0) countEven++;
+    else countNotEven++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел - {countEven}");
+Console.WriteLine($"Количество нечетных чисел - {countNotEven}");

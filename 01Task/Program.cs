12f23dff2fb7ef8118ebdf29ed09f53f5c0b58e8
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-16 11:21:08 +0300

    Программа выполняет два различных вывода массива, состоящего из 8 элементов. В первом варианте элементы массива задаются явно, во время его объявления, во втором варианте задается только размер массива, а наполняется он циклом, где в каждый элемент массива записывается случайно сгенерированное число от 0 до 99

---
+++ 01Task/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+
+int[] Array = new int[8]{0,3,5,1,6,8,4,3};
+for(int i = 0; i<Array.Length;i++)
+{
+    Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
+double[] Array1 = new double[8];
+for(int i = 0; i<Array1.Length; i++)
+{
+    Array1[i] = new Random().Next(0,100);
+    Console.Write($"{Array1[i]} ");
+}

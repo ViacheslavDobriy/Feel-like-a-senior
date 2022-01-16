01ccb82edd20a126c0f98dd7a797913ccb0bcfd0
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-16 11:28:56 +0300

    Программа выводит на экран элементы массива, в которые записываются случайно сгенерированные 0 и 1.

---
+++ 02Task/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+double[] Array = new double[8];
+for(int i = 0; i<Array.Length; i++)
+{
+    Array[i] = new Random().Next(0,2);
+    Console.Write($"{Array[i]} ");
+}

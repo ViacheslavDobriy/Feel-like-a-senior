bdb0335dd37b724140d560f304ccace31c0c3200
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-16 12:14:00 +0300

    Программа задает массив из 15 элементов, затем как бы переворачивает массив и выдает перевернутый массив на экран

---
+++ 04Task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+double[] Array = new double[15];
+for(int i = 0; i<Array.Length; i++)
+{
+    Array[i] = new Random().Next(0,51);
+    Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
+double Remember = 0;
+for(int i = 0; i<=Array.Length/2; i++)
+{
+    Remember = Array[i];
+    Array[i] = Array[Array.Length-1-i];
+    Array[Array.Length-1-i] = Remember;
+}
+for(int i = 0; i<Array.Length; i++)
+{
+    Console.Write($"{Array[i]} ");
+}

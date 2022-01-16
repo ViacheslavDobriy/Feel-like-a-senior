8bc06e33885632d6aa879583035c7a6795d8bd83
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-16 13:27:28 +0300

    Программа выдает на экран количество чисел, взятых из массива, которые соответствуют отрезку [10,99]

---
+++ 07Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+double[] Array = new double[123];
+for(int i = 0; i<Array.Length; i++)
+{
+    Array[i] = new Random().Next(0,1000);
+    Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
+double number = 0;
+for(int i = 0; i < Array.Length; i++)
+{
+    if(Array[i]>=10 && Array[i]<=99) number++;
+}
+Console.WriteLine($"Количество элементов массива из отрезка от 10 до 99 - {number}");
